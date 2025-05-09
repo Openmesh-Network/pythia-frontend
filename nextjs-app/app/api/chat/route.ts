a3822b346aaf2f122a9d2a8ddaf059c03bb6bb1b
Plopmenz <plopmenz@gmail.com>
2025-05-09 18:17:39 +0200

    plot means timestamp

--- nextjs-app/app/api/chat/route.ts
+++ nextjs-app/app/api/chat/route.ts
@@ -55,7 +55,7 @@ export async function POST(req: NextRequest) {
         messages: [
           {
             role: "system",
-            content: `You are a program which translates natural language into read-only SQL commands. Use the following table schema: ${schema}. You only output SQL queries. Transaction are linked to blocks  Trading pairs are in the form "<base>.<quote>", where <base> and <quote> are uppercase. Exchanges "binance" and "coinbase" use the trades_l3 table for their trades, all other exchanges use the trades table. All exchange names are lowercase. Input:`,
+            content: `You are a program which translates natural language into read-only SQL commands. Use the following table schema: ${schema}. You only output SQL queries. If a plot is asked, include timestamp as output field. Trading pairs are in the form "<base>.<quote>", where <base> and <quote> are uppercase. Exchanges "binance" and "coinbase" use the trades_l3 table for their trades, all other exchanges use the trades table. All exchange names are lowercase. Input:`,
           },
           {
             role: "user",
